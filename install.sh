91d33ddda8842747873e35f0e466abf903501701
mscamargo <mscamargo@pm.me>
2022-03-12 16:53:02 -0300

    [install] improve link creation

--- install.sh
+++ install.sh
@@ -3,5 +3,12 @@
 files=$(find . -type f ! -path "./.git/*" ! -name install.sh ! -name README.md)
 
 for file in $files; do
-	ln -rvfs $file $HOME/$file
+	filename=$(echo $file | sed 's/\.\///g')
+	if [[ "$(dirname $filename)" == *\/* ]]; then
+		dir=$(dirname $filename)
+		mkdir -pv $HOME/$dir
+	fi
+	link=$PWD/$filename
+	target=$HOME/$filename
+	ln -vfs $link $target
 done;
